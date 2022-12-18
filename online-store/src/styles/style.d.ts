2ca87a4da9a5b77eb357e8ebd9dd43db46116a3e
YanaPronko <YaniaR@tut.by>
2022-12-18 19:27:13 +0300

    Updates

---
+++ online-store/src/styles/style.d.ts
@@ -0,0 +1,10 @@
+import './constants.scss';
+import './mixins.scss';
+import './placeholder.scss';
+import './base.scss';
+import './logo.scss';
+import './header.scss';
+import './aside.scss';
+import './catalog.scss';
+import './productPage.scss';
+import './footer.scss';
