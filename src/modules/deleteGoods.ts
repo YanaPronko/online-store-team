4afbc58af5836a4d134f4df3d4caec4c68c3b401
d1elit <dropdox12@gmail.com>
2023-01-19 19:05:06 +0300

    style: delete comments, add empty lines

--- src/modules/deleteGoods.ts
+++ src/modules/deleteGoods.ts
@@ -3,7 +3,6 @@ import { item } from "../containers/catalog/catalog";
 import { setStorage } from "./updateStorage";
 import { clearCart } from "./clearCart";
 
-
 export function deleteProductFromCart(array: item[], ind: number): void {
   array.splice(ind, 1);  
   if (array.length === 0) {
